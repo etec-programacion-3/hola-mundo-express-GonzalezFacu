27576c81249d64f49c58c77bc88304db838bb1a5
Facundo <fp.gonzalez@alumno.etec.um.edu.ar>
2024-06-05 11:31:02 -0300

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import express from 'express';
 
 const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Ingrese el nombre en la URL');
+});
+
+app.get('/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send(`Hola ${nombre}`);
 });
 
 app.listen(3000, () => {
